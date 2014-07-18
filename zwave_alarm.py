22488d7cf196b46370a48090fb1dede41d058687
PeterClaydon <peter.claydon@continuumbridge.com>
2014-07-18 19:40:56 +0100

    Just something to put on Github

---
+++ zwave_alarm.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# tempo.py
+# Copyright (C) ContinuumBridge Limited, 2014 - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Peter Claydon
+#
+ModuleName              = "zwave_alarm"
+BATTERY_CHECK_INTERVAL   = 300      # How often to check battery (secs)
+
+import sys
+import time
+import os
+from pprint import pprint
+import logging
+from cbcommslib import CbAdaptor
+from cbconfig import *
+from twisted.internet import threads
+from twisted.internet import reactor
+
+class Adaptor(CbAdaptor):
+    def __init__(self, argv):
+        logging.basicConfig(filename=CB_LOGFILE,level=CB_LOGGING_LEVEL,format='%(asctime)s %(message)s')
+        self.status = "ok"
+        self.state = "stopped"
+        # super's __init__ must be called:
+        #super(Adaptor, self).__init__(argv)
+        CbAdaptor.__init__(self, argv)
+ 
+    def setState(self, action):
+        if self.state == "stopped":
+            if action == "starting":
+                self.state = "starting"
+        elif self.state == "starting":
+            if action == "inUse":
+                self.state = "activate"
+        if self.state == "activate":
+            reactor.callLater(0, self.poll)
+            self.state = "running"
+        # error is only ever set from the running state, so set back to running if error is cleared
+        if action == "error":
+            self.state == "error"
+        elif action == "clear_error":
+            self.state = "running"
+        logging.debug("%s %s state = %s", ModuleName, self.id, self.state)
+        msg = {"id": self.id,
+               "status": "state",
+               "state": self.state}
+        self.sendManagerMessage(msg)
+
+    def reportState(self, state):
+        logging.debug("%s %s Switch state = %s", ModuleName, self.id, state)
+        msg = {"id": self.id,
+               "timeStamp": time.time(),
+               "content": "switch_state",
+               "data": state}
+        for a in self.apps:
+            self.sendMessage(msg, a)
+
+    def sendParameter(self, parameter, data, timeStamp):
+        msg = {"id": self.id,
+               "content": parameter,
+               "data": data,
+               "timeStamp": timeStamp}
+        for a in self.apps[parameter]:
+            reactor.callFromThread(self.sendMessage, msg, a)
+
+    def onStop(self):
+        # Mainly caters for situation where adaptor is told to stop while it is starting
+        pass
+
+    def checkAllProcessed(self, appID):
+        self.processedApps.append(appID)
+        found = True
+        for a in self.appInstances:
+            if a not in self.processedApps:
+                found = False
+        if found:
+            self.setState("inUse")
+
+    def onOff(self, s):
+        if s == "on":
+            return "255"
+        else:
+            return "0"
+
+    def checkBattery(self):
+        cmd = {"id": self.id,
+               "request": "post",
+               "address": self.addr,
+               "instance": "0",
+               "commandClass": "128",
+               "action": "Get",
+               "value": ""
+              }
+        self.sendZwaveMessage(cmd)
+        reactor.callLater(BATTERY_CHECK_INTERVAL, self.checkBattery)
+
+    def onZwaveMessage(self, message):
+        #logging.debug("%s %s onZwaveMessage, message: %s", ModuleName, self.id, str(message))
+        if message["content"] == "init":
+            cmd = {"id": self.id,
+                   "request": "get",
+                   "address": self.addr,
+                   "instance": "0",
+                   "commandClass": "48",
+                   "value": "1"
+                  }
+            self.sendZwaveMessage(cmd)
+            cmd = {"id": self.id,
+                   "request": "get",
+                   "address": self.addr,
+                   "instance": "0",
+                   "commandClass": "128",
+                   "value": "1"
+                  }
+            self.sendZwaveMessage(cmd)
+            reactor.callLater(10, self.checkBattery)
+        elif message["content"] == "parameter":
+            logging.debug("%s %s onZwaveMessage, data: %s", ModuleName, self.id, str(pprint(message["data"])))
+            try:
+                level = message["data"]["level"]["value"]
+                logging.debug("%s %s onZwaveMessage, level: %s", ModuleName, self.id, level)
+            except:
+                logging.debug("%s %s onZwaveMessage,  no level", ModuleName, self.id)
+
+    def onAppInit(self, message):
+        logging.debug("%s %s %s onAppInit, req = %s", ModuleName, self.id, self.friendly_name, message)
+        resp = {"name": self.name,
+                "id": self.id,
+                "status": "ok",
+                "functions": [{"parameter": "binary_sensor",
+                               "type": "motion"}],
+                "content": "functions"}
+        self.sendMessage(resp, message["id"])
+        self.setState("running")
+
+    def onAppCommand(self, message):
+        logging.debug("%s %s %s onAppCommand, req = %s", ModuleName, self.id, self.friendly_name, message)
+        if "data" not in message:
+            logging.warning("%s %s %s app message without data: %s", ModuleName, self.id, self.friendly_name, message)
+        elif message["data"] != "on" and message["data"] != "off":
+            logging.warning("%s %s %s app switch state must be on or off: %s", ModuleName, self.id, self.friendly_name, message)
+        else:
+            self.switch(message["data"])
+
+    def onConfigureMessage(self, config):
+        """Config is based on what apps are to be connected.
+            May be called again if there is a new configuration, which
+            could be because a new app has been added.
+        """
+        logging.debug("%s onConfigureMessage, config: %s", ModuleName, config)
+        self.setState("starting")
+
+if __name__ == '__main__':
+    adaptor = Adaptor(sys.argv)
